21fe09c9500b69d5e0ee13236ebb794415abb461
HenriqueHartmann <alemaohartmann@gmail.com>
2018-10-13 02:38:54 -0300

    Delete rodape.php

--- tcc/app/views/Template/rodape.php
+++
@@ -1,8 +0,0 @@
-<div class="ui inverted all vertical footer segment">
-    <div class="ui center aligned container">
-        <h3 class="item">Instituto Federal Catarinense</h3>
-        <a class="item rod" href="https://www.google.com.br/maps/place/Instituto+Federal+Catarinense+-+Campus+Araquari/@-26.394753,-48.7379352,15z/data=!4m5!3m4!1s0x0:0x34c75ce16022aa6a!8m2!3d-26.394753!4d-48.7379352">Site Map</a>
-    </div>
-</div>
-</body>
-</html>
